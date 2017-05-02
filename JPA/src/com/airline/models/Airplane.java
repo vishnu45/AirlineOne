8024c5005713157963e0499172bbc537e66d7ec1
vishnu45 <vishnu4v5@gmail.com>
2017-05-01 21:47:13 -0400

    ADD: airplane and flight entity

---
+++ JPA/src/com/airline/models/Airplane.java
@@ -0,0 +1,72 @@
+package com.airline.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Entity implementation class for Entity: Airplane
+ *
+ */
+@Entity
+
+public class Airplane implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public Airplane() {
+		super();
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer Id;
+
+	private String planeMake;
+
+	private String modelName;
+
+	private Integer seatingCapacity;
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getPlaneMake() {
+		return planeMake;
+	}
+
+	public void setPlaneMake(String planeMake) {
+		this.planeMake = planeMake;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public Integer getSeatingCapacity() {
+		return seatingCapacity;
+	}
+
+	public void setSeatingCapacity(Integer seatingCapacity) {
+		this.seatingCapacity = seatingCapacity;
+	}
+
+	@Override
+	public String toString() {
+		return "Airplane [Id=" + Id + ", planeMake=" + planeMake + ", modelName=" + modelName + ", seatingCapacity="
+				+ seatingCapacity + "]";
+	}
+
+}
---
+++ JPA/src/com/airline/models/Flight.java
@@ -0,0 +1,91 @@
+package com.airline.models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * Entity implementation class for Entity: Flight
+ *
+ */
+@Entity
+
+public class Flight implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Flight() {
+		super();
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Enumerated(EnumType.STRING)
+	private FlightDestinations flightOrigin;
+	
+	@Enumerated(EnumType.STRING)
+	private FlightDestinations flightDestination;
+	
+	private Integer price;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date flightTime;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public FlightDestinations getFlightOrigin() {
+		return flightOrigin;
+	}
+
+	public void setFlightOrigin(FlightDestinations flightOrigin) {
+		this.flightOrigin = flightOrigin;
+	}
+
+	public FlightDestinations getFlightDestination() {
+		return flightDestination;
+	}
+
+	public void setFlightDestination(FlightDestinations flightDestination) {
+		this.flightDestination = flightDestination;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Date getFlightTime() {
+		return flightTime;
+	}
+
+	public void setFlightTime(Date flightTime) {
+		this.flightTime = flightTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Flight [id=" + id + ", flightOrigin=" + flightOrigin + ", flightDestination=" + flightDestination
+				+ ", price=" + price + ", flightTime=" + flightTime + "]";
+	}
+   
+}
