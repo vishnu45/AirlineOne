8ec4ba1bc4b46614933373c3d5d327e6343d2fb3
vishnu45 <vishnu4v5@gmail.com>
2017-05-02 05:41:54 -0400

    ADD: used named query to retrieve pilot, flight by id

--- JPA/src/com/airline/service/FlightService.java
+++ JPA/src/com/airline/service/FlightService.java
@@ -4,9 +4,11 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import com.airline.models.Airplane;
 import com.airline.models.Flight;
+import com.airline.models.Pilot;
 
 /**
  * Session Bean implementation class FlightService
@@ -32,6 +34,25 @@ public class FlightService {
     public void addFlight(Flight f, Airplane a) {
     	em.persist(f);
     	em.persist(a);
+    }
+    
+    public void addPilotToFlight(String pilotId, String flightId) {
+    	// use the named queries
+    	// arg1 - namedQuery, arg2 - class name of flight entity
+    	// based on the flightId passed into the method, the flight object is pulled up
+    	TypedQuery<Flight> fQuery = em.createNamedQuery("Flight.findById", Flight.class);
+    	
+    	// set the id parameter in the namedQuery with that of the flight being passed
+    	// to retrieve the exact flight
+    	// parse the string flightId to Integer
+    	fQuery.setParameter("id", Integer.parseInt(flightId));
+    	
+    	// get the flight object for the id passed
+    	Flight f = fQuery.getSingleResult();
+    	
+    	TypedQuery<Pilot> pQuery = em.createNamedQuery("Pilot.findById", Pilot.class);
+    	pQuery.setParameter("id", Integer.parseInt(pilotId));
+    	Pilot p = pQuery.getSingleResult();
     }
 
 }
