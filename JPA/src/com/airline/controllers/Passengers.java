967ea1814de7cdceae1cc546d0e1344338aca086
vishnu45 <vishnu4v5@gmail.com>
2017-05-03 06:17:55 -0400

    ADD: passenger list service & servlett

---
+++ JPA/src/com/airline/controllers/Passengers.java
@@ -0,0 +1,58 @@
+package com.airline.controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.airline.models.Flight;
+import com.airline.models.Passenger;
+import com.airline.service.PassengerService;
+
+/**
+ * Servlet implementation class Passengers
+ */
+@WebServlet("/Passengers")
+public class Passengers extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	PassengerService fs;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Passengers() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		List<Passenger> pList = (List<Passenger>) fs.getPassengers();
+		// save flights onto a attribute of request
+		request.setAttribute("passenger_list", pList);
+		
+		// to dispatch flight details to a JSP
+//		RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/passenger_list.jsp");
+//		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- JPA/src/com/airline/service/PassengerService.java
+++ JPA/src/com/airline/service/PassengerService.java
@@ -1,9 +1,12 @@
 package com.airline.service;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import com.airline.models.Passenger;
 
@@ -29,6 +32,13 @@ public class PassengerService {
     public void addPassenger(Passenger p) {
     	
     	em.persist(p);
+    }
+    
+    // to get list of all passengers
+    public List<Passenger> getPassengers() {
+    	TypedQuery<Passenger> query = em.createQuery("SELECT p from Passenger p",Passenger.class);
+    	List<Passenger> pList = query.getResultList();
+    	return pList;
     }
 
 }
