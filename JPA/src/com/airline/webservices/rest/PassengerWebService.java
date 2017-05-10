b031fe368e4ea149b7f3ed56aad889517046c7a6
vishnu45 <vishnu4v5@gmail.com>
2017-05-10 06:46:18 -0400

    ADD: jax-rs webservice initialize for passengers & flights

---
+++ JPA/src/com/airline/webservices/rest/PassengerWebService.java
@@ -0,0 +1,31 @@
+package com.airline.webservices.rest;
+
+import javax.ejb.EJB;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+import com.airline.service.PassengerService;
+
+@Path("/passengers")
+public class PassengerWebService {
+
+	// to interact with the datasoruce we configured on the glassfish derby server
+	@PersistenceContext(unitName = "airline")
+	EntityManager em;
+	
+	@EJB
+	PassengerService ps;
+	
+	// for jax-rs webserivces context
+	// useful for creating URLs to resources on the server (ex: flight resource)
+	@Context
+	UriInfo pUriInfo;
+	
+	public PassengerWebService() {
+		
+	}
+	
+}
