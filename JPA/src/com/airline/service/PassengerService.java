8d6428b53cbac487252160445a58d894fd1e460a
vishnu45 <vishnu4v5@gmail.com>
2017-05-10 05:20:03 -0400

    ADD: add flight tickets to the passenger

--- JPA/src/com/airline/service/PassengerService.java
+++ JPA/src/com/airline/service/PassengerService.java
@@ -7,7 +7,11 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
+import com.airline.models.Flight;
 import com.airline.models.Passenger;
 
 /**
@@ -37,6 +41,35 @@ public class PassengerService {
     // add flight for the passenger
     public void addFlightTicketToPassenger(String flightId, String passengerId) {
     	
+    	// Get the passenger by Id
+    	CriteriaBuilder builder = em.getCriteriaBuilder();
+    	CriteriaQuery<Passenger> cqPassenger = builder.createQuery(Passenger.class);    	
+    	Root<Passenger> pRoot = cqPassenger.from(Passenger.class);
+    	// select passenger only for the Id we provided which is of integer type
+    	cqPassenger.select(pRoot).where(builder.equal(pRoot.get("id").as(Integer.class), passengerId));
+    	
+    	TypedQuery<Passenger> pQuery = em.createQuery(cqPassenger);
+    	Passenger p = pQuery.getSingleResult();
+    	
+    	// Get the flight by Id
+    	builder = em.getCriteriaBuilder();
+    	CriteriaQuery<Flight> cqFlight = builder.createQuery(Flight.class);    	
+    	Root<Flight> fRoot = cqFlight.from(Flight.class);
+    	// select flight only for the Id we provided which is of integer type
+    	cqFlight.select(fRoot).where(builder.equal(fRoot.get("id").as(Integer.class), flightId));
+    	
+    	TypedQuery<Flight> fQuery = em.createQuery(cqFlight);
+    	Flight f = fQuery.getSingleResult();
+    	
+    	// Link flight with passenger
+    	// get list of flights for the passenger
+    	List<Flight> fList = p.getFlights();
+    	fList.add(f);
+    	
+    	// set the list of flights as the new list
+    	p.setFlights(fList);
+    	// add to list of passengers for the given flight
+    	f.getPassengers().add(p);
     }
     
     // to get list of all passengers
